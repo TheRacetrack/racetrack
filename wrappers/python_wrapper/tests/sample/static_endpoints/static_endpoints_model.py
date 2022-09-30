e678d80d86e8de9276f7316b09dd649765392b21
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2022-09-30 14:23:21 +0200

    Move out python3 and docker-http job types to plugin repos

--- wrappers/python_wrapper/tests/sample/static_endpoints/static_endpoints_model.py
+++
@@ -1,9 +0,0 @@
-class StaticEndpoints:
-    def perform(self):
-        pass
-
-    def static_endpoints(self):
-        return {
-            '/xrai': ('sample/static_endpoints/xrai.yaml', 'application/x-yaml'),
-            '/docs': './sample/static_endpoints/docs',
-        }
