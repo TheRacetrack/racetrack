db23f04a768f886d72923581758683808bd3afa3
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2022-10-27 14:16:44 +0200

    Support plugins volume on blob-fuse filesystem

--- racetrack_commons/racetrack_commons/plugin/engine.py
+++ racetrack_commons/racetrack_commons/plugin/engine.py
@@ -152,7 +152,10 @@ class PluginEngine:
         tmp_zip = Path(self.plugins_dir) / f'tmp_{random.randint(0, 999999)}.zip'
         if not tmp_zip.is_file():
             tmp_zip.touch()
-            tmp_zip.chmod(mode=0o666)
+            try:
+                tmp_zip.chmod(mode=0o666)
+            except PermissionError:
+                logger.warning(f'Can\'t change permissions of file {tmp_zip}')
         tmp_zip.write_bytes(file_bytes)
         
         plugin_data = load_plugin_from_zip(tmp_zip)
@@ -211,7 +214,10 @@ class PluginEngine:
         change_file = Path(self.plugins_dir) / LAST_CHANGE_FILE
         if not change_file.is_file():
             change_file.touch()
-            change_file.chmod(mode=0o666)
+            try:
+                change_file.chmod(mode=0o666)
+            except PermissionError:
+                logger.warning(f'Can\'t change permissions of file {change_file}')
         change_file.write_text(f'{self.last_change_timestamp}')
 
     @property
