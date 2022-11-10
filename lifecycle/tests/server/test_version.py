7ff65a6a996a830efdcbb021700a22eb315829f2
Ireneusz Szulc <45496492+iszulcdeepsense@users.noreply.github.com>
2022-11-10 10:21:48 +0100

    Fix test_health_version_endpoint unit test (#86)

--- lifecycle/tests/server/test_version.py
+++ lifecycle/tests/server/test_version.py
@@ -9,12 +9,13 @@ from lifecycle.server.api import create_fastapi_app
 
 def test_health_version_endpoint():
     os.environ['GIT_VERSION'] = '0.0.1-g32c4b29-dirty'
+    os.environ['DJANGO_DB_TYPE'] = 'sqlite'
     fastapi_app = create_fastapi_app(Config(), PluginEngine())
 
     client = TestClient(fastapi_app)
 
     response = client.get('/health')
-    assert response.status_code == 200
+    assert response.status_code == 200, f'Wrong response: {response.content}'
     obj = response.json()
     assert obj['git_version'] == '0.0.1-g32c4b29-dirty'
     assert obj['live'] is True
