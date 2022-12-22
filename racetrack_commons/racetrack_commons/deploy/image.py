f9d4d99dae137dcd51e4765850385c95a1f2b10f
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2022-12-22 10:25:14 +0100

    Include module number in image name

--- racetrack_commons/racetrack_commons/deploy/image.py
+++ racetrack_commons/racetrack_commons/deploy/image.py
@@ -2,9 +2,12 @@ from racetrack_client.utils.url import join_paths
 
 
 def get_fatman_image(docker_registry: str, registry_namespace: str, name: str, tag: str, module_index: int = 0) -> str:
-    """Return full name of Fatman entrypoint image"""
+    """
+    Return full name of Fatman image.
+    If a fatman is composed of multiple containers, the module number is included.
+    """
     if module_index == 0:
         image_type = 'fatman-entrypoint'
     else:
-        image_type = f'fatman-entrypoint-{module_index}'
+        image_type = f'fatman-{module_index}'
     return join_paths(docker_registry, registry_namespace, image_type, f'{name}:{tag}')
