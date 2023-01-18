d6f89edede87f3720a28611d2598d16ab6badb7c
Ireneusz Szulc <45496492+iszulcdeepsense@users.noreply.github.com>
2023-01-18 14:38:22 +0100

    Rephrase Racetrack server\'s URL
    Co-authored-by: Thor K. <thorkik@gmail.com>

--- racetrack_client/racetrack_client/main.py
+++ racetrack_client/racetrack_client/main.py
@@ -75,7 +75,7 @@ def main():
 
     # racetrack move
     parser_move = subparsers.add_parser('move', help='Move fatman from one infrastructure target to another fatman instance')
-    parser_move.add_argument('--remote', type=str, help='URL to Racetrack server or alias name')
+    parser_move.add_argument('--remote', type=str, help='Racetrack server\'s URL or alias name')
     parser_move.add_argument('--name', required=True, type=str, help='fatman name to move out')
     parser_move.add_argument('--version', required=True, type=str, help='fatman version to move out')
     parser_move.add_argument('--infrastructure', required=True, type=str, help='infrastructure target to move on')
