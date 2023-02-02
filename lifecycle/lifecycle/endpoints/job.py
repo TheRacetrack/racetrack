165ffb5cef261ed2a0c01c612e299e7018ac6efe
Thor Kikkenborg <thorkik@gmail.com>
2023-02-02 13:07:15 +0100

    One more degreased file

--- lifecycle/lifecycle/endpoints/job.py
+++ lifecycle/lifecycle/endpoints/job.py
@@ -5,38 +5,38 @@ from pydantic import BaseModel, Field
 
 from lifecycle.auth.check import check_auth
 from lifecycle.config import Config
-from lifecycle.deployer.redeploy import redeploy_fatman, reprovision_fatman, move_fatman
-from lifecycle.fatman.graph import build_fatman_dependencies_graph
-from lifecycle.fatman.registry import (
-    delete_fatman,
-    list_fatman_families,
+from lifecycle.deployer.redeploy import redeploy_job, reprovision_job, move_job
+from lifecycle.job.graph import build_job_dependencies_graph
+from lifecycle.job.registry import (
+    delete_job,
+    list_job_families,
     list_job_registry,
-    read_versioned_fatman,
+    read_versioned_job,
 )
-from lifecycle.fatman.public_endpoints import read_active_fatman_public_endpoints
-from lifecycle.fatman.logs import read_build_logs, read_runtime_logs
+from lifecycle.job.public_endpoints import read_active_job_public_endpoints
+from lifecycle.job.logs import read_build_logs, read_runtime_logs
 from lifecycle.auth.authenticate import get_username_from_token
 from racetrack_commons.entities.dto import FatmanDto, FatmanFamilyDto
 from racetrack_commons.plugin.engine import PluginEngine
 from racetrack_commons.auth.scope import AuthScope
 
 
-def setup_fatman_endpoints(api: APIRouter, config: Config, plugin_engine: PluginEngine):
+def setup_job_endpoints(api: APIRouter, config: Config, plugin_engine: PluginEngine):
 
-    class FatmanResultModel(BaseModel):
+    class JobResultModel(BaseModel):
         id: Optional[str] = Field(
             default=None,
-            description='ID of fatman',
+            description='ID of Job',
             example='00000000-1111-2222-3333-444444444444',
         )
         name: Optional[str] = Field(
             default=None,
-            description='name of Fatman',
+            description='name of Job',
             example='adder',
         )
         version: Optional[str] = Field(
             default=None,
-            description='version of Fatman',
+            description='version of Job',
             example='0.0.1',
         )
         deployed_by: Optional[str] = Field(
@@ -46,7 +46,7 @@ def setup_fatman_endpoints(api: APIRouter, config: Config, plugin_engine: Plugin
         )
         manifest: Optional[Dict[str, Any]] = Field(
             default=None,
-            description='Manifest - build recipe for a Fatman',
+            description='Manifest - build recipe for a Job',
             example={
                 'name': 'adder',
                 'lang': 'python3',
@@ -73,8 +73,8 @@ def setup_fatman_endpoints(api: APIRouter, config: Config, plugin_engine: Plugin
         )
         internal_name: Optional[str] = Field(
             default=None,
-            description='internal name of the fatman',
-            example='fatman-adder-v-0-0-1',
+            description='internal name of the Job',
+            example='job-adder-v-0-0-1',
         )
         create_time: Optional[int] = Field(
             default=None,
@@ -87,75 +87,75 @@ def setup_fatman_endpoints(api: APIRouter, config: Config, plugin_engine: Plugin
             example=1000000,
         )
 
-    class MoveFatmanPayload(BaseModel):
+    class MoveJobPayload(BaseModel):
         infrastructure_target: str = Field(description='text content of configuration file')
 
-    @api.get('/fatman')
-    def _list_all_fatmen(request: Request) -> List[FatmanDto]:
-        """Get list of deployed Fatman"""
-        auth_subject = check_auth(request, scope=AuthScope.READ_FATMAN)
+    @api.get('/job')
+    def _list_all_fatmen(request: Request) -> List[JobDto]:
+        """Get list of deployed Jobs"""
+        auth_subject = check_auth(request, scope=AuthScope.READ_JOB)
         return list_job_registry(config, auth_subject)
 
     @api.get('/fatman_family')
-    def _get_fatman_family(request: Request) -> List[FatmanFamilyDto]:
-        """Get list of deployed Fatman Families (names regardless version)"""
-        auth_subject = check_auth(request, scope=AuthScope.READ_FATMAN)
-        return list_fatman_families(auth_subject)
+    def _get_job_family(request: Request) -> List[JobFamilyDto]:
+        """Get list of deployed Job Families (names regardless version)"""
+        auth_subject = check_auth(request, scope=AuthScope.READ_JOB)
+        return list_job_families(auth_subject)
 
-    @api.get('/fatman_graph')
-    def _get_fatman_graph(request: Request):
-        """Get Fatman dependencies graph"""
-        auth_subject = check_auth(request, scope=AuthScope.READ_FATMAN)
-        return build_fatman_dependencies_graph(config, auth_subject)
+    @api.get('/job_graph')
+    def _get_job_graph(request: Request):
+        """Get Job dependencies graph"""
+        auth_subject = check_auth(request, scope=AuthScope.READ_JOB)
+        return build_job_dependencies_graph(config, auth_subject)
 
-    @api.get('/fatman/{fatman_name}/{fatman_version}', response_model=FatmanResultModel)
-    def _get_fatman(fatman_name: str, fatman_version: str, request: Request):
-        """Get details of particular Fatman"""
-        check_auth(request, fatman_name=fatman_name, fatman_version=fatman_version, scope=AuthScope.READ_FATMAN)
-        return read_versioned_fatman(fatman_name, fatman_version, config)
+    @api.get('/job/{job_name}/{job_version}', response_model=JobResultModel)
+    def _get_job(job_name: str, job_version: str, request: Request):
+        """Get details of particular Job"""
+        check_auth(request, job_name=job_name, job_version=job_version, scope=AuthScope.READ_JOB)
+        return read_versioned_job(job_name, job_version, config)
 
-    @api.delete('/fatman/{fatman_name}/{fatman_version}')
-    def _delete_fatman(fatman_name: str, fatman_version: str, request: Request):
-        """Delete Fatman; expects specific version (can't be latest)"""
-        check_auth(request, fatman_name=fatman_name, fatman_version=fatman_version, scope=AuthScope.DELETE_FATMAN)
+    @api.delete('/job/{job_name}/{job_version}')
+    def _delete_job(job_name: str, job_version: str, request: Request):
+        """Delete Job; expects specific version (can't be latest)"""
+        check_auth(request, job_name=job_name, job_version=job_version, scope=AuthScope.DELETE_JOB)
         username = get_username_from_token(request)
-        return delete_fatman(fatman_name, fatman_version, config, username, plugin_engine)
+        return delete_job(job_name, job_version, config, username, plugin_engine)
 
-    @api.post('/fatman/{fatman_name}/{fatman_version}/redeploy')
-    def _redeploy_fatman(fatman_name: str, fatman_version: str, request: Request):
-        """Deploy specific Fatman image once again (build and provision)"""
-        auth_subject = check_auth(request, fatman_name=fatman_name, fatman_version=fatman_version, scope=AuthScope.DEPLOY_FATMAN)
+    @api.post('/job/{job_name}/{job_version}/redeploy')
+    def _redeploy_job(job_name: str, job_version: str, request: Request):
+        """Deploy specific Job image once again (build and provision)"""
+        auth_subject = check_auth(request, job_name=job_name, job_version=job_version, scope=AuthScope.DEPLOY_JOB)
         username = get_username_from_token(request)
-        return redeploy_fatman(fatman_name, fatman_version, config, plugin_engine, username, auth_subject)
+        return redeploy_job(job_name, job_version, config, plugin_engine, username, auth_subject)
 
-    @api.post('/fatman/{fatman_name}/{fatman_version}/reprovision')
-    def _reprovision_fatman(fatman_name: str, fatman_version: str, request: Request):
-        """Provision specific Fatman image once again to a cluster"""
-        auth_subject = check_auth(request, fatman_name=fatman_name, fatman_version=fatman_version, scope=AuthScope.DEPLOY_FATMAN)
+    @api.post('/job/{job_name}/{job_version}/reprovision')
+    def _reprovision_job(job_name: str, job_version: str, request: Request):
+        """Provision specific Job image once again to a cluster"""
+        auth_subject = check_auth(request, job_name=job_name, job_version=job_version, scope=AuthScope.DEPLOY_JOB)
         username = get_username_from_token(request)
-        return reprovision_fatman(fatman_name, fatman_version, config, plugin_engine, username, auth_subject)
+        return reprovision_job(job_name, job_version, config, plugin_engine, username, auth_subject)
 
-    @api.post('/fatman/{fatman_name}/{fatman_version}/move')
-    def _move_fatman(fatman_name: str, fatman_version: str, payload: MoveFatmanPayload, request: Request):
-        """Move fatman from one infrastructure target to another"""
-        auth_subject = check_auth(request, fatman_name=fatman_name, fatman_version=fatman_version, scope=AuthScope.DEPLOY_FATMAN)
+    @api.post('/job/{job_name}/{job_version}/move')
+    def _move_job(job_name: str, job_version: str, payload: MoveJobPayload, request: Request):
+        """Move Job from one infrastructure target to another"""
+        auth_subject = check_auth(request, job_name=job_name, job_version=job_version, scope=AuthScope.DEPLOY_JOB)
         username = get_username_from_token(request)
-        return move_fatman(fatman_name, fatman_version, payload.infrastructure_target, config, plugin_engine, username, auth_subject)
+        return move_job(job_name, job_version, payload.infrastructure_target, config, plugin_engine, username, auth_subject)
 
-    @api.get('/fatman/{fatman_name}/{fatman_version}/logs')
-    def _get_fatman_logs(request: Request, fatman_name: str, fatman_version: str, tail: int = 20):
-        """Get last logs of particular Fatman"""
-        check_auth(request, fatman_name=fatman_name, fatman_version=fatman_version, scope=AuthScope.READ_FATMAN)
-        return {'logs': read_runtime_logs(fatman_name, fatman_version, tail, config, plugin_engine)}
+    @api.get('/job/{job_name}/{job_version}/logs')
+    def _get_job_logs(request: Request, job_name: str, job_version: str, tail: int = 20):
+        """Get last logs of particular Job"""
+        check_auth(request, job_name=job_name, job_version=job_version, scope=AuthScope.READ_JOB)
+        return {'logs': read_runtime_logs(job_name, job_version, tail, config, plugin_engine)}
 
-    @api.get('/fatman/{fatman_name}/{fatman_version}/build-logs')
-    def _get_fatman_build_logs(request: Request, fatman_name: str, fatman_version: str, tail: int = 0):
-        """Get build logs of Fatman deployment attempt"""
-        check_auth(request, fatman_name=fatman_name, fatman_version=fatman_version, scope=AuthScope.READ_FATMAN)
-        return {'logs': read_build_logs(fatman_name, fatman_version, tail)}
+    @api.get('/job/{job_name}/{job_version}/build-logs')
+    def _get_job_build_logs(request: Request, job_name: str, job_version: str, tail: int = 0):
+        """Get build logs of Job deployment attempt"""
+        check_auth(request, job_name=job_name, job_version=job_version, scope=AuthScope.READ_JOB)
+        return {'logs': read_build_logs(job_name, job_version, tail)}
 
-    @api.get("/fatman/{fatman_name}/{fatman_version}/public-endpoints")
-    def _get_fatman_public_endpoints(request: Request, fatman_name: str, fatman_version: str):
-        """Get list of active public endpoints that can be accessed without authentication for a particular Fatman"""
-        check_auth(request, fatman_name=fatman_name, fatman_version=fatman_version, scope=AuthScope.READ_FATMAN)
-        return read_active_fatman_public_endpoints(fatman_name, fatman_version)
+    @api.get("/job/{job_name}/{job_version}/public-endpoints")
+    def _get_job_public_endpoints(request: Request, job_name: str, job_version: str):
+        """Get list of active public endpoints that can be accessed without authentication for a particular Job"""
+        check_auth(request, job_name=job_name, job_version=job_version, scope=AuthScope.READ_JOB)
+        return read_active_job_public_endpoints(job_name, job_version)
