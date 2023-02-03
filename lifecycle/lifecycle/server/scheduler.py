a4869e4815660b331e44d3beb8174d359a3c0944
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-02-03 15:35:33 +0100

    Populate metrics jobs config

--- lifecycle/lifecycle/server/scheduler.py
+++ lifecycle/lifecycle/server/scheduler.py
@@ -6,6 +6,7 @@ import schedule
 from lifecycle.config import Config
 from lifecycle.fatman.registry import sync_registry_fatmen
 from lifecycle.fatman.reconcile import reconcile_fatmen
+from lifecycle.supervisor.metrics import populate_metrics_jobs
 from racetrack_commons.plugin.engine import PluginEngine
 from racetrack_client.log.context_error import ContextError
 from racetrack_client.log.exception import log_exception
@@ -23,6 +24,7 @@ def schedule_tasks_sync(config: Config, plugin_engine: PluginEngine):
 def _schedule_tasks(config: Config, plugin_engine: PluginEngine):
     schedule.every(1).minutes.do(sync_registry_fatmen, config=config, plugin_engine=plugin_engine)
     schedule.every(10).minutes.do(reconcile_fatmen, config=config, plugin_engine=plugin_engine)
+    schedule.every(1).minutes.do(populate_metrics_jobs, config=config)
 
 
 def _scheduler_worker():
