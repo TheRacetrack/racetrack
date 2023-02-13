c4b9213a073e82c11b2b871d31742ebd9b468ddf
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-02-13 11:05:01 +0100

    Reset os envs after tests

--- lifecycle/tests/commons/test_database.py
+++ lifecycle/tests/commons/test_database.py
@@ -5,22 +5,26 @@ from racetrack_commons.database.database import populate_database_settings
 
 
 def test_populate_database_settings():
-    os.environ['DJANGO_DB_TYPE'] = 'postgres'
-    os.environ['POSTGRES_DB'] = 'racetrack-{CLUSTER_NAME}'
-    os.environ['POSTGRES_USER'] = 'johny'
-    os.environ['POSTGRES_PASSWORD'] = '***'
-    os.environ['POSTGRES_HOST'] = 'localhost'
-    os.environ['POSTGRES_PORT'] = '5432'
-    os.environ['CLUSTER_FQDN'] = 'racetrack.dev-c1.cluster.example.com'
-    os.environ['RACETRACK_SUBDOMAIN'] = 'racetrack'
+    try:
+        os.environ['DJANGO_DB_TYPE'] = 'postgres'
+        os.environ['POSTGRES_DB'] = 'racetrack-{CLUSTER_NAME}'
+        os.environ['POSTGRES_USER'] = 'johny'
+        os.environ['POSTGRES_PASSWORD'] = '***'
+        os.environ['POSTGRES_HOST'] = 'localhost'
+        os.environ['POSTGRES_PORT'] = '5432'
+        os.environ['CLUSTER_FQDN'] = 'racetrack.dev-c1.cluster.example.com'
+        os.environ['RACETRACK_SUBDOMAIN'] = 'racetrack'
 
-    databases = populate_database_settings(Path('.'))
-    assert databases['default'] == {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'racetrack-dev-c1',
-        'USER': 'johny',
-        'PASSWORD': '***',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'CONN_MAX_AGE': 60,
-    }
+        databases = populate_database_settings(Path('.'))
+        assert databases['default'] == {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'racetrack-dev-c1',
+            'USER': 'johny',
+            'PASSWORD': '***',
+            'HOST': 'localhost',
+            'PORT': '5432',
+            'CONN_MAX_AGE': 60,
+        }
+
+    finally:
+        del os.environ['DJANGO_DB_TYPE']
