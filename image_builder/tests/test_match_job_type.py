cf2ad2783aa0cdb76e51f8ad3d662f8a186f5686
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-03-09 17:09:14 +0100

    Fix matching latest job type version

--- image_builder/tests/test_match_job_type.py
+++ image_builder/tests/test_match_job_type.py
@@ -10,3 +10,7 @@ def test_match_job_type_version():
     assert match_job_type_version('labber:1.30.*-3.9-bullseye', [
         'labber:1.30.1-3.9-bullseye', 'labber:1.30.9-3.8-bullseye', 'labber:1.30.99-3.9-buster',
     ]) == 'labber:1.30.1-3.9-bullseye'
+
+    assert match_job_type_version('python3:latest', [
+        'python3:2.5.1', 'python3:2.11.1', 'python3:2.5.99',
+    ]) == 'python3:2.11.1'
