699ec469aa74e78aab633c59d895ae381933a454
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-03-13 15:45:59 +0100

    Reset cookie after regenerating a token

--- dashboard/dashboard/middleware.py
+++ dashboard/dashboard/middleware.py
@@ -3,6 +3,7 @@ import os
 from urllib.parse import quote
 
 from django.db.utils import OperationalError, InterfaceError
+from django.http import HttpResponse
 
 from racetrack_client.log.context_error import ContextError
 from racetrack_client.utils.auth import RT_AUTH_HEADER
@@ -40,28 +41,40 @@ class UserCookieMiddleWare(object):
             if auth_token is None:
                 logging.error("UserCookieMiddleware: user_auth from session is empty")
                 return response
-
-            # if cookie value contains /, it would get unnecessarily double quoted, so encode
-            value = quote(auth_token, safe="")
-
-            # Domain defines the host to which the cookie will be sent.
-            # Empty domain means that cookie will be created for current host, not including subdomains.
-            # if specified, then subdomains are included.
-            response.set_cookie(
-                key=RT_AUTH_HEADER,
-                value=value,
-                path='/',
-                domain=cookie_domain,
-                max_age=60 * 60 * 24 * 365,  # one year of cookie lifetime (in seconds), also makes cookie persist between browser restarts
-                samesite='lax',  # cookie is sent only when user access origin site or navigates to it, prevents CSRF
-                httponly=True,  # prevents client-side JavaScript read access
-                secure=True)  # cookie is sent only when request is made with https (except on localhost)
+        
+            set_auth_token_cookie(auth_token, response)
 
         elif not user_authenticated:
             if request.COOKIES.get(RT_AUTH_HEADER):
                 response.delete_cookie(
                     key=RT_AUTH_HEADER,
                     path='/',
-                    domain=cookie_domain)
+                    domain=_get_cookie_domain())
 
         return response
+
+
+def set_auth_token_cookie(auth_token: str, response: HttpResponse):
+    # if cookie value contains /, it would get unnecessarily double quoted, so encode
+    value = quote(auth_token, safe="")
+
+    # Domain defines the host to which the cookie will be sent.
+    # Empty domain means that cookie will be created for current host, not including subdomains.
+    # if specified, then subdomains are included.
+    response.set_cookie(
+        key=RT_AUTH_HEADER,
+        value=value,
+        path='/',
+        domain=_get_cookie_domain(),
+        max_age=60 * 60 * 24 * 365,  # one year of cookie lifetime (in seconds), also makes cookie persist between browser restarts
+        samesite='lax',  # cookie is sent only when user access origin site or navigates to it, prevents CSRF
+        httponly=True,  # prevents client-side JavaScript read access
+        secure=True)  # cookie is sent only when request is made with https (except on localhost)
+
+
+def _get_cookie_domain() -> str | None:
+    cookie_domain = os.environ.get('CLUSTER_FQDN', None)
+    if not cookie_domain:
+        return cookie_domain
+    racetrack_subdomain = os.environ.get('RACETRACK_SUBDOMAIN', 'racetrack')
+    return f'{racetrack_subdomain}.{cookie_domain}'
