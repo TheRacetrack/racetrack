afaa2fe1ab88f2774f6f831600844c7b309b5366
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-03-15 17:42:20 +0100

    Reuse connection pool

--- tests/performance/response_time_test.py
+++ tests/performance/response_time_test.py
@@ -1,5 +1,6 @@
 import os
 import statistics
+import time
 
 import httpx
 from dotenv import load_dotenv
@@ -19,20 +20,31 @@ headers = {
     'X-Racetrack-Auth': auth_token,
 }
 
+reuse_connection = True
+
 print(f'Testing URL {url}')
 
 durations = []
+client = httpx.Client()
 try:
     for i in range(attempts):
-        response = httpx.post(url, timeout=30, headers=headers, content=payload)
+
+        if reuse_connection:
+            response = client.post(url, timeout=30, headers=headers, content=payload)
+
+        else:
+            response = httpx.post(url, timeout=30, headers=headers, content=payload)
+
         duration = response.elapsed.total_seconds()
 
+        time.sleep(1)
         response.raise_for_status()
-
         durations.append(duration)
         print(f'Attempt #{i+1} - request duration: {duration*1000} ms')
 except KeyboardInterrupt:
     pass
+finally:
+    client.close()
 
 average = sum(durations) / len(durations)
 min_duration = min(durations)
