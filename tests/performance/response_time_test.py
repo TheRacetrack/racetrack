22d7eff1366476cb74178748b016e7850fdc4994
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-03-16 16:45:45 +0100

    Fix reading reuhse_connection var

--- tests/performance/response_time_test.py
+++ tests/performance/response_time_test.py
@@ -14,9 +14,10 @@ attempts = int(os.environ.get('attempts', 100))
 payload = os.environ.get('payload')
 auth_header = os.environ.get('auth_header', 'X-Racetrack-Auth')
 auth_token = os.environ.get('auth_token', '')
-reuse_connection: bool = os.environ.get('auth_header', 'true').lower() in {'true', '1'}
+reuse_connection: bool = os.environ.get('reuse_connection', 'true').lower() in {'true', '1'}
 
 print(f'Testing URL {url}')
+print(f'attempts: {attempts}, reuse_connection (connection pool): {reuse_connection}, payload: {payload}')
 
 durations = []
 headers = {
@@ -37,7 +38,6 @@ try:
 
         duration = response.elapsed.total_seconds()
 
-        time.sleep(1)
         response.raise_for_status()
         durations.append(duration)
         print(f'Attempt #{i+1} - request duration: {duration*1000:.2f} ms')
@@ -50,7 +50,6 @@ average = sum(durations) / len(durations)
 min_duration = min(durations)
 max_duration = max(durations)
 median = statistics.median(durations)
-
 print(f'----------')
 print(f'Requests: {len(durations)}')
 print(f'Median: {median*1000:.2f} ms')
