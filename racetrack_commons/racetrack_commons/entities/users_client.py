851f60b6cdc1ebdfe2c11d186995f082bbfef8b6
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-04-04 14:59:12 +0200

    Build json dict correctly

--- racetrack_commons/racetrack_commons/entities/users_client.py
+++ racetrack_commons/racetrack_commons/entities/users_client.py
@@ -9,11 +9,11 @@ class UserAccountClient:
 
     def register_user(self, username: str, password: str):
         self.lc_client.request('post', f'/api/v1/users/register',
-                               json={username: username, password: password})
+                               json={'username': username, 'password': password})
 
     def login_user(self, username: str, password: str) -> UserProfileDto:
         response = self.lc_client.request_dict('post', f'/api/v1/users/login',
-                                               json={username: username, password: password})
+                                               json={'username': username, 'password': password})
         return parse_dict_datamodel(response, UserProfileDto)
 
     def validate_token(self) -> UserProfileDto:
@@ -22,7 +22,7 @@ class UserAccountClient:
 
     def change_password(self, old_password: str, new_password: str):
         self.lc_client.request('put', f'/api/v1/users/change_password',
-                               json={old_password: old_password, new_password: new_password})
+                               json={'old_password': old_password, 'new_password': new_password})
 
     def regen_user_token(self) -> str:
         response = self.lc_client.request('post', f'/api/v1/auth/token/user/regenerate')
