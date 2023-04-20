18ea5f40533dec29cbb31b06c8ea1d0c276b3945
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-04-20 10:07:49 +0200

    Clear cookie after logging out

--- dashboard/dashboard-front/src/views/JobsListView.vue
+++ dashboard/dashboard-front/src/views/JobsListView.vue
@@ -1,6 +1,41 @@
 <script setup lang="ts">
+import { reactive } from 'vue'
+import axios from "axios"
+import { AUTH_HEADER } from '@/services/RequestUtils'
+import { userData } from '@/services/UserDataStore'
+import { ToastService } from '@/services/ToastService';
+
+const jobsData = reactive({
+  jobs: [],
+})
+
+function fetchJobs() {
+    axios.get(`/api/job/list`, {
+        headers: {
+            [AUTH_HEADER]: userData.authToken,
+        },
+    }).then(response => {
+
+        jobsData.jobs = response.data.jobs
+
+    }).catch(err => {
+        ToastService.showRequestError(`Failed to fetch the jobs`, err)
+    })
+}
+
+fetchJobs()
 </script>
 
 <template>
-  <h1>Jobs</h1>
+  <q-card>
+    <q-card-section>
+      <div class="text-h6">
+        Jobs
+      </div>
+    </q-card-section>
+
+    <q-card-section class="q-pt-none">
+      {{ jobsData.jobs }}
+    </q-card-section>
+  </q-card>
 </template>
--- dashboard/dashboard-front/src/views/LogoutView.vue
+++ dashboard/dashboard-front/src/views/LogoutView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+import axios from "axios"
 import { deleteUserData } from '@/services/UserDataStore'
 import { ToastService } from '@/services/ToastService'
 
@@ -8,7 +9,13 @@ const router = useRouter()
 function logout() {
   deleteUserData()
   router.push({ name: 'login' })
-  ToastService.success(`You're logged out.`)
+
+  axios.get(`/api/accounts/logout`)
+  .then(response => {
+    ToastService.success(`You're logged out.`)
+  }).catch(err => {
+    ToastService.showRequestError(`Failed to log out`, err)
+  })
 }
 
 logout()
