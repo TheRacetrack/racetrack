583797d3ab246948559554e15c4c07682e16f131
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-05-04 11:42:15 +0200

    Use latest Node LTS version

--- dashboard/dashboard-front/nvm-activate.sh
+++ dashboard/dashboard-front/nvm-activate.sh
@@ -6,4 +6,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm use v16.20.0
+nvm use v18.16
