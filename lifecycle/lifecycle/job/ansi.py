44b598cfe7de1b8814c19db4a02c5c0c482c4099
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-05-08 16:05:58 +0200

    Rename to strip_ansi_colors

--- lifecycle/lifecycle/job/ansi.py
+++ lifecycle/lifecycle/job/ansi.py
@@ -1,6 +1,6 @@
 import re
 
 
-def remove_ansi_sequences(content: str) -> str:
+def strip_ansi_colors(content: str) -> str:
     """Remove ANSI escape codes controlling font colors"""
     return re.sub(r'\x1b\[\d+(;\d+)?m', '', content)
