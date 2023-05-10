e93551eb5d00ac0cb75fd20f150783d6c30cf440
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-05-10 15:28:02 +0200

    Store jobs ordering in local storage, print number of jobs per family

--- dashboard/dashboard-front/src/components/jobs/JobsListView.vue
+++ dashboard/dashboard-front/src/components/jobs/JobsListView.vue
@@ -65,6 +65,7 @@ function populateJobsTree() {
                 label: familyName,
                 key: `job-family:${familyName}`,
                 type: 'job-family',
+                childrenCount: jobs.length,
                 children: jobs.map(job => ({
                     label: `v${job.version}`,
                     key: `job:${job.name}-${job.version}`,
@@ -111,6 +112,11 @@ function collapseAll() {
     jobsQTreeRef.value?.collapseAll()
 }
 
+function updatedJobOrder() {
+    localStorage.setItem('jobs.order', JSON.stringify(jobOrder.value))
+    populateJobsTree()
+}
+
 watch(jobsData, () => {
     populateJobsTree()
 })
@@ -122,13 +128,24 @@ watch(jobsTree, () => {
 })
 
 onMounted(() => {
+    const storedOrder = localStorage.getItem('jobs.order')
+    if (storedOrder) {
+        try {
+            jobOrder.value = JSON.parse(storedOrder) as JobOrder
+        } catch(e) {
+            console.error(e)
+        }
+    }
+
     fetchJobs()
 })
 </script>
 <template>
     <q-card>
         <q-card-section class="q-pb-none">
-            <div class="text-h6">Jobs ({{jobsCount}})</div>
+            <div class="text-h6">Jobs
+                <q-badge outline color="grey">{{jobsCount}}</q-badge>
+            </div>
         </q-card-section>
         
         <q-card-section>
@@ -154,19 +171,19 @@ onMounted(() => {
                         <q-btn-dropdown rounded flat color="grey-7" icon="sort" dropdown-icon="none">
                             <q-list>
                                 <q-item>
-                                    <q-radio v-model="jobOrder" :val="JobOrder.ByName" @click="populateJobsTree()" v-close-popup
+                                    <q-radio v-model="jobOrder" :val="JobOrder.ByName" @click="updatedJobOrder()" v-close-popup
                                         label="Sort by name"/>
                                 </q-item>
                                 <q-item>
-                                    <q-radio v-model="jobOrder" :val="JobOrder.ByLatestFamily" @click="populateJobsTree()" v-close-popup
+                                    <q-radio v-model="jobOrder" :val="JobOrder.ByLatestFamily" @click="updatedJobOrder()" v-close-popup
                                         label="Sort by latest family"/>
                                 </q-item>
                                 <q-item>
-                                    <q-radio v-model="jobOrder" :val="JobOrder.ByLatestJob" @click="populateJobsTree()" v-close-popup
+                                    <q-radio v-model="jobOrder" :val="JobOrder.ByLatestJob" @click="updatedJobOrder()" v-close-popup
                                         label="Sort by latest job"/>
                                 </q-item>
                                 <q-item>
-                                    <q-radio v-model="jobOrder" :val="JobOrder.ByStatus" @click="populateJobsTree()" v-close-popup
+                                    <q-radio v-model="jobOrder" :val="JobOrder.ByStatus" @click="updatedJobOrder()" v-close-popup
                                         label="Sort by status"/>
                                 </q-item>
                             </q-list>
@@ -190,6 +207,11 @@ onMounted(() => {
                                         {{ prop.node.label }}
                                         <JobStatus :status="getJobByKey(prop.node.key)?.status" short />
                                     </div>
+                                </template>
+                                <template v-else-if="prop.node.type == 'job-family'">
+                                    {{ prop.node.label }}
+                                    &nbsp;
+                                    <q-badge outline color="grey">{{prop.node.childrenCount}}</q-badge>
                                 </template>
                                 <template v-else>
                                     {{ prop.node.label }}
