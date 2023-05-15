ca93b8e319d7e016d8ff3070612073f2197a1d4c
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-05-15 10:52:32 +0200

    Format 404 error properly

--- dashboard/dashboard-front/src/utils/error.ts
+++ dashboard/dashboard-front/src/utils/error.ts
@@ -7,6 +7,15 @@ export function extractErrorDetails(err: any): string {
             return data.error
         if (data?.type)
             return data.type
+        if (err.message) {
+            const method = err.config?.method?.toUpperCase()
+            if (data) {
+                const dataJson = JSON.stringify(data)
+                return `${err.message}: ${method} ${err.config?.url}: ${dataJson}`
+            } else {
+                return `${err.message}: ${method} ${err.config?.url}`
+            }
+        }
     }
     if (err.hasOwnProperty('xhr')){
         const xhr = err.xhr
