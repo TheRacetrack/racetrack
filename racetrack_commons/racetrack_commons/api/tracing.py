be2a32312342514b7f3f5bbd4d48cbef8c435389
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-05-17 16:19:22 +0200

    Add caller name to request logger

--- racetrack_commons/racetrack_commons/api/tracing.py
+++ racetrack_commons/racetrack_commons/api/tracing.py
@@ -4,6 +4,7 @@ from fastapi import Request
 
 from racetrack_client.log.exception import get_exc_info_details
 from racetrack_client.log.logs import get_logger
+from racetrack_commons.api.debug import is_env_flag_enabled
 
 logger = get_logger(__name__)
 
@@ -12,15 +13,26 @@ class RequestTracingLogger(logging.LoggerAdapter):
     """Logging adapter adding request tracing ID to log messages"""
     def process(self, msg, kwargs):
         tracing_id = self.extra['tracing_id']
-        if tracing_id is None:
-            return msg, kwargs
-        else:
+        caller_name = self.extra.get('caller_name')
+        caller_enabled = is_env_flag_enabled('LOG_CALLER_NAME')
+        if tracing_id and caller_name and caller_enabled:
+            return f"[{tracing_id}, {caller_name}] {msg}", kwargs
+        elif tracing_id:
             return f"[{tracing_id}] {msg}", kwargs
+        elif caller_name and caller_enabled:
+            return f"[{caller_name}] {msg}", kwargs
+        else:
+            return msg, kwargs
 
 
 def get_tracing_header_name() -> str:
     """Return name of HTTP request header that contains the tracing ID"""
     return os.environ.get('REQUEST_TRACING_HEADER', 'X-Request-Tracing-Id')
+
+
+def get_caller_header_name() -> str:
+    """Return name of HTTP request header that contains the caller name"""
+    return os.environ.get('CALLER_NAME_HEADER', 'X-Caller-Name')
 
 
 def log_request_exception_with_tracing(request: Request, e: BaseException):
@@ -29,8 +41,13 @@ def log_request_exception_with_tracing(request: Request, e: BaseException):
         log_message = get_exc_info_details(ex_type, e, tb)
 
         tracing_header = get_tracing_header_name()
+        caller_header = get_caller_header_name()
         tracing_id = request.headers.get(tracing_header)
-        request_logger = RequestTracingLogger(logger, {'tracing_id': tracing_id})
+        caller_name = request.headers.get(caller_header)
+        request_logger = RequestTracingLogger(logger, {
+            'tracing_id': tracing_id,
+            'caller_name': caller_name,
+        })
         request_logger.error(log_message)
         
     except BaseException as e:
