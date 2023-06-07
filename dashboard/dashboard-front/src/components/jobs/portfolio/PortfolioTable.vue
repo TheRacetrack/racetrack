6a1ebc8fee4a6887fc7e58d5fe5861b8dfa9de11
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-06-07 18:25:34 +0200

    Use QTable in Portfolio tab

--- dashboard/dashboard-front/src/components/jobs/portfolio/PortfolioTable.vue
+++ dashboard/dashboard-front/src/components/jobs/portfolio/PortfolioTable.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, onUpdated, ref, type Ref } from 'vue'
+import { computed, onMounted, onUpdated, ref, type Ref } from 'vue'
 import { toastService } from '@/services/ToastService'
 import { formatTimestampIso8601 } from '@/utils/time'
 import { formatDecimalNumber } from '@/utils/string'
@@ -8,7 +8,50 @@ import DeleteJobButton from '@/components/jobs/DeleteJobButton.vue'
 
 const portfolioJobs: Ref<PortfolioJob[]> = ref([])
 
+const portfolioRows: Ref<PortfolioRow[]> = computed(() => {
+    return portfolioJobs.value.map(job => (
+        job as PortfolioRow
+    ))
+})
+
+const visibleColumns = ref([
+    'family-name', 'version', 'status', 'job-type-version', 'deployed-by', 'last-updated-ago', 'last-called-ago',
+])
+
+const pagination = ref({
+    sortBy: 'family-name',
+    descending: false,
+    page: 1,
+    rowsPerPage: 25,
+})
+
+const filter = ref('')
+
 interface PortfolioJob {
+    name: string
+    version: string
+    status: string
+    create_time: number
+    update_time: number
+    id: string
+    manifest?: string
+    internal_name?: string
+    pub_url?: string
+    error?: string
+    image_tag?: string
+    deployed_by?: string
+    last_call_time?: number
+    infrastructure_target?: string
+    replica_internal_names?: string[]
+    job_type_version: string
+    purge_score: number
+    purge_reasons: string
+    purge_newer_versions: number
+    update_time_days_ago: number
+    last_call_time_days_ago: number
+}
+
+interface PortfolioRow {
     name: string
     version: string
     status: string
@@ -104,9 +147,165 @@ onMounted(() => {
 onUpdated(() => {
     tf.init()
 })
+
+
+const columns = [
+    {
+        name: 'family-name',
+        label: 'Family name',
+        align: 'left',
+        field: (row: PortfolioRow) => row.name,
+        sortable: true,
+    },
+    {
+        name: 'version',
+        label: 'Job version',
+        align: 'left',
+        field: (row: PortfolioRow) => row.version,
+        sortable: true,
+    },
+    {
+        name: 'status',
+        label: 'Status',
+        align: 'left',
+        field: (row: PortfolioRow) => row.status.toUpperCase(),
+        sortable: true,
+    },
+    {
+        name: 'job-type-version',
+        label: 'Job type version',
+        align: 'left',
+        field: (row: PortfolioRow) => row.job_type_version,
+        sortable: true,
+    },
+    {
+        name: 'deployed-by',
+        label: 'Deployed by',
+        align: 'left',
+        field: (row: PortfolioRow) => row.deployed_by || '',
+        sortable: true,
+    },
+    {
+        name: 'last-updated-ago',
+        label: 'Last updated ago [days]',
+        align: 'left',
+        field: (row: PortfolioRow) => formatDecimalNumber(row.update_time_days_ago),
+        sortable: true,
+    },
+    {
+        name: 'last-called-ago',
+        label: 'Last called ago [days]',
+        align: 'left',
+        field: (row: PortfolioRow) => formatDecimalNumber(row.last_call_time_days_ago),
+        sortable: true,
+    },
+    {
+        name: 'purge-reasons',
+        label: 'Purge reasons',
+        align: 'left',
+        field: (row: PortfolioRow) => row.purge_reasons,
+        sortable: true,
+    },
+    {
+        name: 'purge-score',
+        label: 'Purge score',
+        align: 'left',
+        field: (row: PortfolioRow) => formatDecimalNumber(row.purge_score),
+        sortable: true,
+    },
+    {
+        name: 'newer-versions',
+        label: 'Newer versions',
+        align: 'left',
+        field: (row: PortfolioRow) => row.purge_newer_versions,
+        sortable: true,
+    },
+    {
+        name: 'last-call-time',
+        label: 'Last calll time',
+        align: 'left',
+        field: (row: PortfolioRow) => formatTimestampIso8601(row.last_call_time),
+        sortable: true,
+    },
+    {
+        name: 'udpate-time',
+        label: 'Update time',
+        align: 'left',
+        field: (row: PortfolioRow) => formatTimestampIso8601(row.update_time),
+        sortable: true,
+    },
+    {
+        name: 'creation-time',
+        label: 'Creation time',
+        align: 'left',
+        field: (row: PortfolioRow) => formatTimestampIso8601(row.create_time),
+        sortable: true,
+    },
+    {
+        name: 'infrastructure-target',
+        label: 'Infrastructure target',
+        align: 'left',
+        field: (row: PortfolioRow) => row.infrastructure_target,
+        sortable: true,
+    },
+    {
+        name: 'actions',
+        label: 'Actions',
+        align: 'left',
+        field: (row: PortfolioRow) => "",
+        sortable: true,
+    },
+]
+
 </script>
 
 <template>
+    <q-table
+      flat bordered
+      :rows="portfolioRows"
+      :columns="columns"
+      row-key="id"
+      :visible-columns="visibleColumns"
+      :pagination="pagination"
+      no-data-label="No jobs"
+    >
+        <template v-slot:top-right>
+            <q-input borderless dense debounce="300" v-model="filter" placeholder="Filter">
+                <template v-slot:append>
+                    <q-icon name="search" />
+                </template>
+            </q-input>
+            <q-select
+                v-model="visibleColumns"
+                multiple
+                outlined dense options-dense
+                :display-value="$q.lang.table.columns"
+                emit-value
+                map-options
+                :options="columns"
+                option-value="name"
+                options-cover
+                style="min-width: 150px"
+            />
+        </template>
+        <template v-slot:header="props">
+            <q-tr :props="props">
+                <q-th
+                    v-for="col in props.cols"
+                    :key="col.name"
+                    :props="props"
+                    >
+                    <span class="text-bold">{{ col.label }}</span>
+                </q-th>
+            </q-tr>
+        </template>
+        <template v-slot:body-cell-actions="props">
+            <q-td :props="props">
+                <DeleteJobButton :jobName="props.row.name" :jobVersion="props.row.version" @jobDeleted="fetchJobs()" />
+            </q-td>
+        </template>
+    </q-table>
+
     <table id="table-filter-1" ref="tableRef">
         <thead>
             <tr>
