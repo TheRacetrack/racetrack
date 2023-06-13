57fc90af2e0cd80d4c3113da8a52eead5d57fb8d
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-06-13 13:43:50 +0200

    Add filtering in Qtable

--- dashboard/dashboard-front/src/components/jobs/portfolio/PortfolioTable.vue
+++ dashboard/dashboard-front/src/components/jobs/portfolio/PortfolioTable.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, onMounted, onUpdated, ref, type Ref } from 'vue'
+import { onMounted, ref, type Ref } from 'vue'
+import { type QTableProps } from 'quasar'
 import { toastService } from '@/services/ToastService'
 import { formatTimestampIso8601 } from '@/utils/time'
 import { formatDecimalNumber } from '@/utils/string'
@@ -7,51 +8,18 @@ import { apiClient } from '@/services/ApiClient'
 import DeleteJobButton from '@/components/jobs/DeleteJobButton.vue'
 
 const portfolioJobs: Ref<PortfolioJob[]> = ref([])
-
-const portfolioRows: Ref<PortfolioRow[]> = computed(() => {
-    return portfolioJobs.value.map(job => (
-        job as PortfolioRow
-    ))
-})
-
 const visibleColumns = ref([
     'family-name', 'version', 'status', 'job-type-version', 'deployed-by', 'last-updated-ago', 'last-called-ago',
 ])
-
 const pagination = ref({
     sortBy: 'family-name',
     descending: false,
     page: 1,
     rowsPerPage: 25,
 })
-
 const filter = ref('')
 
 interface PortfolioJob {
-    name: string
-    version: string
-    status: string
-    create_time: number
-    update_time: number
-    id: string
-    manifest?: string
-    internal_name?: string
-    pub_url?: string
-    error?: string
-    image_tag?: string
-    deployed_by?: string
-    last_call_time?: number
-    infrastructure_target?: string
-    replica_internal_names?: string[]
-    job_type_version: string
-    purge_score: number
-    purge_reasons: string
-    purge_newer_versions: number
-    update_time_days_ago: number
-    last_call_time_days_ago: number
-}
-
-interface PortfolioRow {
     name: string
     version: string
     status: string
@@ -83,198 +51,138 @@ function fetchJobs() {
     })
 }
 
-// see https://github.com/koalyptus/TableFilter/wiki/1.0-Configuration
-const tfConfig = {
-    base_path: '/dashboard/ui/assets/tablefilter/',
-    paging: {
-        results_per_page: ['Rows: ', [10, 25, 50, 100]]
-    },
-    auto_filter: { delay: 500 }, // Delay for automatic filtering (milliseconds)
-    state: { // Enable state persistence
-        types: ['local_storage'], // Possible values: 'local_storage' 'hash' or 'cookie'  
-        filters: true, // Persist filters values, enabled by default  
-        columns_visibility: true, // Persist columns visibility  
-        filters_visibility: true, // Persist filters row visibility  
-    },
-    alternate_rows: true,
-    rows_counter: true,
-    toolbar: true,
-    btn_reset: {
-        text: 'Clear'
-    },
-    status_bar: true,
-    col_types: [
-        'string', // 0. family name
-        'string', // 1. version
-        'string', // 2. status
-        'string', // 3. job type version
-        'string', // 4. deployed by
-        'number', // 5. last updated ago
-        'number', // 6. last called ago
-        'string', // 7. purge reasons
-        'number', // 8. purge score
-        'number', // 9. newer versions
-        { type: 'date' }, // 10. last call time
-        { type: 'date' }, // 11. update time
-        { type: 'date' }, // 12. creation time
-        'string', // 13. infrastructure target
-        'none', // 14. actions
-    ],
-    col_12: 'none',
-    no_results_message: {
-        content: 'No results',
-    },
-    extensions: [{
-        name: 'colsVisibility',
-        at_start: [6, 7, 8, 9, 10, 11, 12, 13, 14],
-        text: 'Columns: ',
-        enable_tick_all: true,
-        tick_to_hide: false,
-    }, {
-        name: 'sort'
-    }],
-}
-
-const tableRef: Ref<HTMLElement | null> = ref(null)
-let tf: any = null
-
 onMounted(() => {
     fetchJobs()
-    // @ts-ignore
-    tf = new TableFilter(tableRef.value, tfConfig)
 })
 
-onUpdated(() => {
-    tf.init()
-})
-
-
-const columns = [
+const columns: QTableProps['columns'] = [
     {
         name: 'family-name',
         label: 'Family name',
         align: 'left',
-        field: (row: PortfolioRow) => row.name,
+        field: (row: PortfolioJob) => row.name,
         sortable: true,
     },
     {
         name: 'version',
         label: 'Job version',
         align: 'left',
-        field: (row: PortfolioRow) => row.version,
+        field: (row: PortfolioJob) => row.version,
         sortable: true,
     },
     {
         name: 'status',
         label: 'Status',
         align: 'left',
-        field: (row: PortfolioRow) => row.status.toUpperCase(),
+        field: (row: PortfolioJob) => row.status.toUpperCase(),
         sortable: true,
     },
     {
         name: 'job-type-version',
         label: 'Job type version',
         align: 'left',
-        field: (row: PortfolioRow) => row.job_type_version,
+        field: (row: PortfolioJob) => row.job_type_version,
         sortable: true,
     },
     {
         name: 'deployed-by',
         label: 'Deployed by',
         align: 'left',
-        field: (row: PortfolioRow) => row.deployed_by || '',
+        field: (row: PortfolioJob) => row.deployed_by || '',
         sortable: true,
     },
     {
         name: 'last-updated-ago',
         label: 'Last updated ago [days]',
-        align: 'left',
-        field: (row: PortfolioRow) => formatDecimalNumber(row.update_time_days_ago),
+        align: 'right',
+        field: (row: PortfolioJob) => formatDecimalNumber(row.update_time_days_ago),
         sortable: true,
     },
     {
         name: 'last-called-ago',
         label: 'Last called ago [days]',
-        align: 'left',
-        field: (row: PortfolioRow) => formatDecimalNumber(row.last_call_time_days_ago),
+        align: 'right',
+        field: (row: PortfolioJob) => formatDecimalNumber(row.last_call_time_days_ago),
         sortable: true,
     },
     {
         name: 'purge-reasons',
         label: 'Purge reasons',
         align: 'left',
-        field: (row: PortfolioRow) => row.purge_reasons,
+        field: (row: PortfolioJob) => row.purge_reasons,
         sortable: true,
     },
     {
         name: 'purge-score',
         label: 'Purge score',
-        align: 'left',
-        field: (row: PortfolioRow) => formatDecimalNumber(row.purge_score),
+        align: 'right',
+        field: (row: PortfolioJob) => formatDecimalNumber(row.purge_score),
         sortable: true,
     },
     {
         name: 'newer-versions',
         label: 'Newer versions',
-        align: 'left',
-        field: (row: PortfolioRow) => row.purge_newer_versions,
+        align: 'right',
+        field: (row: PortfolioJob) => row.purge_newer_versions,
         sortable: true,
     },
     {
         name: 'last-call-time',
         label: 'Last calll time',
         align: 'left',
-        field: (row: PortfolioRow) => formatTimestampIso8601(row.last_call_time),
+        field: (row: PortfolioJob) => formatTimestampIso8601(row.last_call_time),
         sortable: true,
     },
     {
         name: 'udpate-time',
         label: 'Update time',
         align: 'left',
-        field: (row: PortfolioRow) => formatTimestampIso8601(row.update_time),
+        field: (row: PortfolioJob) => formatTimestampIso8601(row.update_time),
         sortable: true,
     },
     {
         name: 'creation-time',
         label: 'Creation time',
         align: 'left',
-        field: (row: PortfolioRow) => formatTimestampIso8601(row.create_time),
+        field: (row: PortfolioJob) => formatTimestampIso8601(row.create_time),
         sortable: true,
     },
     {
         name: 'infrastructure-target',
         label: 'Infrastructure target',
         align: 'left',
-        field: (row: PortfolioRow) => row.infrastructure_target,
+        field: (row: PortfolioJob) => row.infrastructure_target,
         sortable: true,
     },
     {
         name: 'actions',
         label: 'Actions',
         align: 'left',
-        field: (row: PortfolioRow) => "",
+        field: (row: PortfolioJob) => "",
         sortable: true,
     },
 ]
 
+const headerTooltips: Record<string, string> = {
+    'version': 'Revision of the source code of the service',
+    'job-type-version': 'Revision of the language wrapper standard',
+    'purge-reasons': 'Suggestions explaining why job is a candidate for removal',
+    'purge-score': 'Assessed penalty points representing usability of a job. A higher value means a better candidate for removal',
+}
 </script>
 
 <template>
     <q-table
       flat bordered
-      :rows="portfolioRows"
+      :rows="portfolioJobs"
       :columns="columns"
       row-key="id"
       :visible-columns="visibleColumns"
       :pagination="pagination"
+      :filter="filter"
       no-data-label="No jobs"
     >
-        <template v-slot:top-right>
-            <q-input borderless dense debounce="300" v-model="filter" placeholder="Filter">
-                <template v-slot:append>
-                    <q-icon name="search" />
-                </template>
-            </q-input>
+        <template v-slot:top-left>
             <q-select
                 v-model="visibleColumns"
                 multiple
@@ -287,6 +195,11 @@ const columns = [
                 options-cover
                 style="min-width: 150px"
             />
+            <q-input outlined dense debounce="300" v-model="filter" placeholder="Filter">
+                <template v-slot:append>
+                    <q-icon name="search" />
+                </template>
+            </q-input>
         </template>
         <template v-slot:header="props">
             <q-tr :props="props">
@@ -295,7 +208,10 @@ const columns = [
                     :key="col.name"
                     :props="props"
                     >
-                    <span class="text-bold">{{ col.label }}</span>
+                    <span class="text-bold">
+                        {{ col.label }}
+                        <q-tooltip v-if="headerTooltips[col.name]">{{ headerTooltips[col.name] }}</q-tooltip>
+                    </span>
                 </q-th>
             </q-tr>
         </template>
@@ -305,54 +221,4 @@ const columns = [
             </q-td>
         </template>
     </q-table>
-
-    <table id="table-filter-1" ref="tableRef">
-        <thead>
-            <tr>
-                <th>Family name</th>
-                <th data-bs-toggle="tooltip" data-bs-placement="bottom" title="Revision of the source code of the service">
-                    Job version</th>
-                <th>Status</th>
-                <th data-bs-toggle="tooltip" data-bs-placement="bottom" title="Revision of the language wrapper standard">
-                    Job type version</th>
-                <th>Deployed by</th>
-                <th>Last updated ago [days]</th>
-                <th>Last called ago [days]</th>
-                <th data-bs-toggle="tooltip" data-bs-placement="bottom"
-                    title="Suggestions explaining why job is a candidate for removal">Purge reasons</th>
-                <th data-bs-toggle="tooltip" data-bs-placement="bottom"
-                    title="Assessed penalty points representing usability of a job. A higher value means a better candidate for removal">
-                    Purge score</th>
-                <th>Newer versions</th>
-                <th>Last calll time</th>
-                <th>Update time</th>
-                <th>Creation time</th>
-                <th>Infrastructure target</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            <tr v-for="job in portfolioJobs">
-                <td>{{ job.name }}</td>
-                <td>{{ job.version }}</td>
-                <td>{{ job.status.toUpperCase() }}</td>
-                <td>{{ job.job_type_version }}</td>
-                <td>{{ job.deployed_by || '' }}</td>
-                <td>{{ formatDecimalNumber(job.update_time_days_ago) }}</td>
-                <td>{{ formatDecimalNumber(job.last_call_time_days_ago) }}</td>
-                <td>{{ job.purge_reasons }}</td>
-                <td>{{ formatDecimalNumber(job.purge_score) }}</td>
-                <td>{{ job.purge_newer_versions }}</td>
-                <td>{{ formatTimestampIso8601(job.last_call_time) }}</td>
-                <td>{{ formatTimestampIso8601(job.update_time) }}</td>
-                <td>{{ formatTimestampIso8601(job.create_time) }}</td>
-                <td>{{ job.infrastructure_target }}</td>
-                <td>
-                    <DeleteJobButton :jobName="job.name" :jobVersion="job.version" @jobDeleted="fetchJobs()" />
-                </td>
-            </tr>
-
-        </tbody>
-    </table>
 </template>
