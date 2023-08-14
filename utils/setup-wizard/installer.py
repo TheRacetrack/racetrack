4ce6ff4b0f6532441284428692a446fd0bc809b3
Ireneusz Szulc <45496492+iszulcdeepsense@users.noreply.github.com>
2023-08-14 15:38:48 +0200

    Update utils/setup-wizard/installer.py
    Co-authored-by: Thor K. <thorkik@gmail.com>

--- utils/setup-wizard/installer.py
+++ utils/setup-wizard/installer.py
@@ -20,7 +20,7 @@ def main():
     configure_logs(log_level='debug')
 
     templates_path = Path('kustomize/external')
-    assert templates_path.is_dir(), f"can't find directory: {templates_path}. Make sure you're in the root of repository"
+    assert templates_path.is_dir(), f"can't find directory: {templates_path}. Make sure you're in the root of the repository"
     generated_path = Path('kustomize/generated')
     if generated_path.exists():
         logger.info('cleaning up "generated" directory')
