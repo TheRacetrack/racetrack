1bbb488c540da7735873e4a777fb0f04e4a31a4c
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-08-30 14:25:49 +0200

    Restore remoteGatewayEndpoint

--- pub/gateway.go
+++ pub/gateway.go
@@ -18,6 +18,7 @@ func init() {
 }
 
 const RemoteGatewayTokenHeader = "X-Racetrack-Gateway-Token"
+const JobInternalNameHeader = "X-Racetrack-Job-Internal-Name"
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -139,6 +140,9 @@ func serveGatewayWebsocketCalls(cfg *Config, conn *websocket.Conn, gatewayHost s
 	for {
 		err, fatalError := handleGatewayWebsocketCall(cfg, conn)
 		if fatalError != nil {
+			if websocket.IsCloseError(fatalError, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				break
+			}
 			log.Error("Gateway Websocket error, closing connection", log.Ctx{
 				"error": fatalError,
 			})
@@ -161,6 +165,12 @@ func serveGatewayWebsocketCalls(cfg *Config, conn *websocket.Conn, gatewayHost s
 func handleGatewayWebsocketCall(cfg *Config, conn *websocket.Conn) (err error, fatalError error) {
 	_, message, err := conn.ReadMessage()
 	if err != nil {
+		if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+			return nil, errors.Wrap(err, "Websocket closed unexpectedly")
+		}
+		if websocket.IsCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+			return nil, err
+		}
 		return nil, errors.Wrap(err, "Websocket read failed")
 	}
 	log.Debug("Websocket message received")
@@ -280,4 +290,86 @@ func (l *slaveLifecycleClient) AuthorizeCaller(jobName, jobVersion, endpoint str
 	}
 
 	return response.JobCallAuthData, response.Err
+}
+
+func remoteGatewayEndpoint(c *gin.Context, cfg *Config, jobPath string) {
+	requestId := getRequestTracingId(c.Request, cfg.RequestTracingHeader)
+	logger := log.New(log.Ctx{
+		"requestId": requestId,
+	})
+
+	logger.Info("Incoming forwarding request from master Pub", log.Ctx{"method": c.Request.Method, "path": c.Request.URL.Path})
+	statusCode, err := handleRemoteGatewayRequest(c, cfg, logger, requestId, jobPath)
+	if err != nil {
+		errorStr := err.Error()
+		logger.Error("Proxy request error", log.Ctx{
+			"status": statusCode,
+			"error":  errorStr,
+			"path":   c.Request.URL.Path,
+		})
+		c.JSON(statusCode, gin.H{
+			"error":     fmt.Sprintf("Proxy request error: %s", errorStr),
+			"status":    http.StatusText(statusCode),
+			"requestId": requestId,
+		})
+	}
+}
+
+func handleRemoteGatewayRequest(
+	c *gin.Context,
+	cfg *Config,
+	logger log.Logger,
+	requestId string,
+	jobPath string,
+) (int, error) {
+	if !cfg.RemoteGatewayMode {
+		return http.StatusUnauthorized, errors.New("Forwarding endpoint is only available in remote gateway mode")
+	}
+
+	if c.Request.Method != "POST" && c.Request.Method != "GET" {
+		c.Writer.Header().Set("Allow", "GET, POST")
+		return http.StatusMethodNotAllowed, errors.New("Method not allowed")
+	}
+
+	jobName := c.Param("job")
+	if jobName == "" {
+		return http.StatusBadRequest, errors.New("Couldn't extract job name")
+	}
+	jobVersion := c.Param("version")
+	if jobVersion == "" {
+		return http.StatusBadRequest, errors.New("Couldn't extract job version")
+	}
+
+	gatewayToken := c.Request.Header.Get(RemoteGatewayTokenHeader)
+	if gatewayToken == "" {
+		return http.StatusUnauthorized, errors.Errorf("Pub gateway expects token in %s header", RemoteGatewayTokenHeader)
+	}
+	if gatewayToken != cfg.RemoteGatewayToken {
+		return http.StatusUnauthorized, errors.New("Pub gateway token is invalid")
+	}
+
+	jobInternalName := c.Request.Header.Get(JobInternalNameHeader)
+	if jobInternalName == "" {
+		return http.StatusBadRequest, errors.Errorf("Pub gateway expects job name in %s header", JobInternalNameHeader)
+	}
+
+	job := &JobDetails{
+		Name:         jobName,
+		Version:      jobVersion,
+		InternalName: jobInternalName,
+	}
+	callerName := c.Request.Header.Get(cfg.CallerNameHeader)
+
+	urlPath := JoinURL("/pub/job/", job.Name, job.Version, jobPath)
+	targetUrl := TargetURL(cfg, job, urlPath)
+
+	logger.Debug("Forwarding request to job", log.Ctx{
+		"jobName":         jobName,
+		"jobVersion":      jobVersion,
+		"jobInternalName": jobInternalName,
+		"targetUrl":       targetUrl.String(),
+	})
+
+	ServeReverseProxy(targetUrl, c, job, cfg, logger, requestId, callerName)
+	return 200, nil
 }
--- pub/router.go
+++ pub/router.go
@@ -39,6 +39,13 @@ func ListenAndServe(cfg *Config) error {
 			proxyEndpoint(c, cfg, "")
 		})
 
+		router.Any(baseUrl+"/slave/forward/:job/:version/*path", func(c *gin.Context) {
+			remoteGatewayEndpoint(c, cfg, "/"+c.Param("path"))
+		})
+		router.Any(baseUrl+"/slave/forward/:job/:version", func(c *gin.Context) {
+			remoteGatewayEndpoint(c, cfg, "")
+		})
+
 		router.GET(baseUrl+"/live", liveEndpoint)
 		router.GET(baseUrl+"/ready", readyEndpoint)
 		router.GET(baseUrl+"/health", handlerWithConfig(healthEndpoint, cfg))
