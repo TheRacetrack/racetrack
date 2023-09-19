e519a71b1214b0d18d302400b14f6520dd2c5494
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-09-19 12:31:52 +0200

    Extract errors from exception groups

--- racetrack_commons/racetrack_commons/api/asgi/error_handler.py
+++ racetrack_commons/racetrack_commons/api/asgi/error_handler.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from exceptiongroup import ExceptionGroup
 
 from racetrack_client.log.errors import EntityNotFound, AlreadyExists, ValidationError
 from racetrack_commons.api.tracing import log_request_exception_with_tracing
@@ -65,9 +66,29 @@ def register_error_handlers(api: FastAPI):
     async def catch_all_exceptions_middleware(request: Request, call_next):
         try:
             return await call_next(request)
+        except ExceptionGroup as e:
+            log_request_exception_with_tracing(request, e)
+            error_message, error_type = _upack_error_message(e)
+            return JSONResponse(
+                status_code=500,
+                content={'error': error_message, "type": error_type},
+            )
         except BaseException as error:
             log_request_exception_with_tracing(request, error)
             return JSONResponse(
                 status_code=500,
                 content={'error': str(error), "type": type(error).__name__},
             )
+
+
+def _upack_error_message(e: BaseException) -> tuple[str, str]:
+    if isinstance(e, ExceptionGroup):
+        e_message = f'{e}: '
+        e_type = f'{type(e).__name__}: '
+        for suberror in e.exceptions:
+            sub_message, sub_type = _upack_error_message(suberror)
+            e_message += sub_message
+            e_type += sub_type
+        return e_message, e_type
+
+    return str(e), type(e).__name__
