7cfbce9d5baaa926342350bd34864b7c068feb62
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-09-20 13:15:06 +0200

    Add metric with number of lifecycle api calls

--- racetrack_commons/racetrack_commons/api/metrics.py
+++ racetrack_commons/racetrack_commons/api/metrics.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
-from prometheus_client import Counter
+from prometheus_client import Counter, Histogram
 from prometheus_client.exposition import make_asgi_app
 from prometheus_client.registry import REGISTRY
 from racetrack_commons.api.asgi.proxy import TrailingSlashForwarder
 
 metric_internal_server_errors = Counter(
-    'internal_server_errors',
+    'commons_internal_server_errors',
     'Number of Internal Server Errors (500) caught by error handler',
+)
+metric_request_duration = Histogram(
+    'commons_request_duration',
+    'Duration of API requests',
+    buckets=(.001, .0025, .005, .01, .025, .05, .075, .1, .25, .5, .75, 1.0, 2.5, 5.0, 7.5,
+             10.0, 25.0, 50.0, 75.0, 100.0, 250.0, 500.0, 750.0, 1000.0, float("inf")),
+)
+metric_requests_started = Counter(
+    'commons_requests_started',
+    'Total number of started API requests (may be not finished yet)',
+)
+metric_requests_done = Counter(
+    'commons_requests_done',
+    'Total number of finished API requests (processed and done)',
 )
 
 
