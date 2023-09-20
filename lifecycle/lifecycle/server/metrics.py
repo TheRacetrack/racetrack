d8179c953e919c8ad4df3a6201f8fd9f106ee744
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-09-20 13:45:50 +0200

    Make sure asyncio loop exists

--- lifecycle/lifecycle/server/metrics.py
+++ lifecycle/lifecycle/server/metrics.py
@@ -3,6 +3,7 @@ import collections
 import os
 import socket
 import threading
+from typing import Iterator
 
 import psutil
 from django.conf import settings
@@ -11,7 +12,7 @@ from django.db.utils import OperationalError
 from prometheus_client import Counter, Gauge
 from prometheus_client.core import REGISTRY
 from prometheus_client.registry import Collector
-from prometheus_client.metrics_core import GaugeMetricFamily
+from prometheus_client.metrics_core import GaugeMetricFamily, Metric
 
 metric_requested_job_deployments = Counter('requested_job_deployments', 'Number of requests to deploy job')
 metric_deployed_job = Counter('deployed_job', 'Number of Jobs deployed successfully')
@@ -59,29 +60,41 @@ class DatabaseConnectionCollector(Collector):
 class ServerResourcesCollector(Collector):
     def collect(self):
         metric_metrics_scrapes.inc()
+        yield from collect_active_threads_metric()
+        yield from collect_coroutines_count_metric()
+        yield from collect_tcp_connections_metric()
 
-        metric_name = 'lifecycle_active_threads_count'
-        metric_value = threading.active_count()
-        prometheus_metric = GaugeMetricFamily(metric_name, 'Number of Thread objects currently alive')
+
+def collect_active_threads_metric() -> Iterator[Metric]:
+    metric_name = 'lifecycle_active_threads_count'
+    metric_value = threading.active_count()
+    prometheus_metric = GaugeMetricFamily(metric_name, 'Number of Thread objects currently alive')
+    prometheus_metric.add_sample(metric_name, {}, metric_value)
+    yield prometheus_metric
+
+
+def collect_coroutines_count_metric() -> Iterator[Metric]:
+    metric_name = 'lifecycle_coroutines_count'
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = None
+    if loop is not None and not loop.is_closed():
+        metric_value = len(asyncio.all_tasks(loop))
+        prometheus_metric = GaugeMetricFamily(metric_name, 'Number of async Task objects run by the current loop')
         prometheus_metric.add_sample(metric_name, {}, metric_value)
         yield prometheus_metric
 
-        metric_name = 'lifecycle_coroutines_count'
-        loop = asyncio.get_event_loop()
-        if loop is not None:
-            metric_value = len(asyncio.all_tasks(loop))
-            prometheus_metric = GaugeMetricFamily(metric_name, 'Number of async Task objects run by the current loop')
-            prometheus_metric.add_sample(metric_name, {}, metric_value)
-            yield prometheus_metric
 
-        metric_name = 'lifecycle_tcp_connections_count'
-        tcp_connections = collections.Counter(p.status for p in psutil.net_connections(kind='tcp'))
-        prometheus_metric = GaugeMetricFamily(metric_name, 'Number of open TCP connections by status')
-        for status, count in tcp_connections.items():
-            prometheus_metric.add_sample(metric_name, {
-                'status': status,
-            }, count)
-        yield prometheus_metric
+def collect_tcp_connections_metric() -> Iterator[Metric]:
+    metric_name = 'lifecycle_tcp_connections_count'
+    tcp_connections = collections.Counter(p.status for p in psutil.net_connections(kind='tcp'))
+    prometheus_metric = GaugeMetricFamily(metric_name, 'Number of open TCP connections by status')
+    for status, count in tcp_connections.items():
+        prometheus_metric.add_sample(metric_name, {
+            'status': status,
+        }, count)
+    yield prometheus_metric
 
 
 def is_database_connected() -> bool:
