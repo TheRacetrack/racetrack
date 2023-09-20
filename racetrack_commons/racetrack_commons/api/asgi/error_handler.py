63614f6308f9a5d83f8cba3a77ab81e134c6aa39
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-09-20 17:22:55 +0200

    Use non-async error handlers

--- racetrack_commons/racetrack_commons/api/asgi/error_handler.py
+++ racetrack_commons/racetrack_commons/api/asgi/error_handler.py
@@ -11,7 +11,7 @@ from racetrack_commons.auth.auth import UnauthorizedError
 def register_error_handlers(api: FastAPI):
 
     @api.exception_handler(ValueError)
-    async def value_error_handler(request: Request, error: ValueError):
+    def value_error_handler(request: Request, error: ValueError):
         """Bad Request error"""
         log_request_exception_with_tracing(request, error)
         return JSONResponse(
@@ -20,7 +20,7 @@ def register_error_handlers(api: FastAPI):
         )
 
     @api.exception_handler(UnauthorizedError)
-    async def unauthorized_error_handler(request: Request, error: UnauthorizedError):
+    def unauthorized_error_handler(request: Request, error: UnauthorizedError):
         """Unauthorized error"""
         log_request_exception_with_tracing(request, error)
         return JSONResponse(
@@ -29,7 +29,7 @@ def register_error_handlers(api: FastAPI):
         )
 
     @api.exception_handler(EntityNotFound)
-    async def not_found_error_handler(request: Request, error: EntityNotFound):
+    def not_found_error_handler(request: Request, error: EntityNotFound):
         """Not Found error"""
         log_request_exception_with_tracing(request, error)
         return JSONResponse(
@@ -38,7 +38,7 @@ def register_error_handlers(api: FastAPI):
         )
 
     @api.exception_handler(AlreadyExists)
-    async def conflict_error_handler(request: Request, error: AlreadyExists):
+    def conflict_error_handler(request: Request, error: AlreadyExists):
         """Already Exists - Conflict error"""
         log_request_exception_with_tracing(request, error)
         return JSONResponse(
@@ -47,7 +47,7 @@ def register_error_handlers(api: FastAPI):
         )
 
     @api.exception_handler(ValidationError)
-    async def validation_error_handler(request: Request, error: AlreadyExists):
+    def validation_error_handler(request: Request, error: AlreadyExists):
         """Validation Error"""
         return JSONResponse(
             content={"error": str(error), "type": type(error).__name__},
@@ -55,7 +55,7 @@ def register_error_handlers(api: FastAPI):
         )
 
     @api.exception_handler(Exception)
-    async def default_error_handler(request: Request, error: Exception):
+    def default_error_handler(request: Request, error: Exception):
         """Internal Server error"""
         metric_internal_server_errors.inc()
         log_request_exception_with_tracing(request, error)
@@ -95,11 +95,12 @@ def _upack_error_message(e: BaseException) -> tuple[str, str]:
             sub_messages.append(sub_message)
             sub_types.append(sub_type)
 
-        e_type = f'{type(e).__name__}: ' + ', '.join(sub_types)
         if len(sub_messages) == 1:
             e_message = sub_messages[0]
+            e_type = sub_types[0]
         else:
             e_message = f'{e}: ' + ', '.join(sub_messages)
+            e_type = f'{type(e).__name__}: ' + ', '.join(sub_types)
         return e_message, e_type
 
     return str(e), type(e).__name__
