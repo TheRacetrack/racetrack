3a95c6b48bfe5fc91ac468e77bc0b8d65f29c902
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-09-29 17:17:30 +0200

    Verify database name evaluation

--- postgres/wait-for-postgres.sh
+++ postgres/wait-for-postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB; do
+until pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER; do
     echo "Waiting for ${POSTGRES_HOST}:${POSTGRES_PORT}";
     sleep 1;
 done;
