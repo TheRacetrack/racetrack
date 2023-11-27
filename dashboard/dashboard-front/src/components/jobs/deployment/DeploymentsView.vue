c2a472260ef091bd3677f04a4ed8391c7b4c17e6
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2023-11-27 16:12:52 +0100

    Fetch deployments from API

--- dashboard/dashboard-front/src/components/jobs/deployment/DeploymentsView.vue
+++ dashboard/dashboard-front/src/components/jobs/deployment/DeploymentsView.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
-import { ref, type Ref } from 'vue'
-import { apiClient } from '@/services/ApiClient'
+import { ref, type Ref, onMounted } from 'vue'
 import { toastService } from '@/services/ToastService'
-import {progressService} from "@/services/ProgressService"
-import yaml from 'js-yaml'
+import { apiClient } from '@/services/ApiClient'
+import { timestampPrettyAgo, timestampToLocalTime } from '@/utils/time'
+import {DeploymentDto} from "@/utils/api-schema"
 
+const deploymentsData: Ref<DeploymentDto[]> = ref([] as DeploymentDto[])
+const loading = ref(false)
+
+function fetchDeploymentsData() {
+    loading.value = true
+    apiClient.get<DeploymentDto[]>(`/api/v1/deploy`)
+        .then(response => {
+            deploymentsData.value = response.data
+        }).catch(err => {
+            toastService.showErrorDetails(`Failed to fetch deployments`, err)
+        }).finally(() => {
+            loading.value = false
+        })
+}
+
+onMounted(() => {
+    fetchDeploymentsData()
+})
 </script>
 <template>
     <q-card>
@@ -12,8 +30,35 @@ import yaml from 'js-yaml'
             <q-btn color="primary" push label="Deploy a new job" icon="add" :to="{name: 'deploy-job'}" />
         </q-card-section>
 
-        <q-card-section class="">
+        <q-card-section>
             <div class="text-h6">Deployments</div>
+        </q-card-section>
+
+        <q-card-section>
+            <q-list bordered separator class="rounded-borders">
+
+                <q-item v-if="deploymentsData.length == 0" class="text-grey-6">(empty)</q-item>
+
+                <q-item v-for="item in deploymentsData">
+                    <q-item-section avatar>
+                        <q-spinner color="primary" size="2em" v-if="result.status == 'in_progress'"/>
+                        <q-icon color="positive" name="check_circle" v-if="result.status == 'done'"/>
+                        <q-icon color="negative" name="error" v-if="result.status == 'failed'"/>
+                    </q-item-section>
+
+                    <q-item-section>
+                        <q-item-label>{{item.id}}</q-item-label>
+                        <q-item-label caption>{{item.status}}, {{item.deployed_by}}, {{item.job_name}} {{item.job_version}}</q-item-label>
+                    </q-item-section>
+
+                    <q-item-section side>
+                        <q-item-label>
+                            {{timestampPrettyAgo(item.update_time)}}
+                            <q-tooltip>{{timestampToLocalTime(item.update_time)}}</q-tooltip>
+                        </q-item-label>
+                    </q-item-section>
+                </q-item>
+            </q-list>
         </q-card-section>
 
     </q-card>
