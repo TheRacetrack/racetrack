18f8747bd7fa1b7ba9553d9f412402eca37eda36
Ireneusz Szulc <45496492+iszulcdeepsense@users.noreply.github.com>
2023-12-06 14:34:20 +0100

    Update runner.sh (#389)

--- utils/standalone-wizard/runner.sh
+++ utils/standalone-wizard/runner.sh
@@ -4,7 +4,7 @@ set -e
 echo "Preparing installer…"
 RT_BRANCH="${RT_BRANCH:-master}"
 curl -#o wizard.py https://raw.githubusercontent.com/TheRacetrack/racetrack/${RT_BRANCH}/utils/standalone-wizard/wizard.py
-curl -#o wizard.py https://raw.githubusercontent.com/TheRacetrack/racetrack/${RT_BRANCH}/utils/standalone-wizard/utils.py
+curl -#o utils.py https://raw.githubusercontent.com/TheRacetrack/racetrack/${RT_BRANCH}/utils/standalone-wizard/utils.py
 
 if [ ! -d "venv" ]; then # if dir doesn't exist
   echo "Preparing virtual Python environment…"
