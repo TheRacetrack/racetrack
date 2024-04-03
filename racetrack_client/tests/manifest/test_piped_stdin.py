ef8a192b21005439ae857eccb4482c7359f9d997
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2024-04-03 10:15:33 +0200

    Fix test: manifest/test_piped_stdin.py:22: in test_invalid_manifest_direct_pipe_to_deploy

--- racetrack_client/tests/manifest/test_piped_stdin.py
+++ racetrack_client/tests/manifest/test_piped_stdin.py
@@ -13,11 +13,12 @@ invaild_job_yaml_text = r"""
                 ||     ||
 """
 
+
 def test_invalid_manifest_direct_pipe_to_deploy():
     # Test if piping input works
     runner = typer.testing.CliRunner()
     result = runner.invoke(cli, ['deploy'], input=invaild_job_yaml_text)
-    print( str(result.exception))
+    print(str(result.exception))
     assert result.exit_code != 0, "Command should fail since the manifest is invalid"
-    assert "'str' object has no attribute 'get'" in str(result.exception).lower(), "Unexpected error type"
-
+    error_message = str(result.exception).lower()
+    assert "'str' object has no attribute 'get'" in error_message or "'nonetype' object is not iterable" in error_message, "Unexpected error type"
