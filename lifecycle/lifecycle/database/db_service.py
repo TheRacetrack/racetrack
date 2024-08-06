2b2d571636b2bfe45b6fa1f33b6da1e0d984e533
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2024-08-06 09:26:18 +0200

    Build queries with filters and ordered by

--- lifecycle/lifecycle/database/db_service.py
+++ lifecycle/lifecycle/database/db_service.py
@@ -1,9 +1,10 @@
 import os
-from typing import Any
+from typing import Any, Iterable
 
 from psycopg_pool import ConnectionPool
 from psycopg import Connection, Cursor
-from psycopg.sql import SQL, Literal, Identifier, Composed
+from psycopg.sql import SQL, Literal, Identifier, Placeholder, Composable
+from psycopg.abc import Query
 
 from racetrack_client.log.logs import get_logger
 
@@ -22,7 +23,6 @@ class DbService:
         }
         self.schema = get_schema_name()
 
-        # https://www.psycopg.org/psycopg3/docs/api/pool.html#psycopg_pool.ConnectionPool
         self.connection_pool: ConnectionPool = ConnectionPool(
             min_size=1,  # The minimum number of connection the pool will hold. The pool will actively try to create new connections if some are lost (closed, broken) and will try to never go below min_size
             max_size=max_pool_size,
@@ -42,8 +42,8 @@ class DbService:
     def on_configure_connection(self, connection: Connection) -> None:
         if self.schema:
             with connection.cursor() as cursor:
-                sql = SQL('SET search_path TO {schema}').format(schema=Literal(self.schema))
-                cursor.execute(sql)
+                query = SQL('SET search_path TO {schema}').format(schema=Literal(self.schema))
+                cursor.execute(query)
             connection.commit()
         self.connection_status = True
 
@@ -61,35 +61,119 @@ class DbService:
     def connection_pool_size(self) -> int:
         return self.connection_pool.get_stats()['pool_size']
 
-    def execute_sql(self, sql: Composed, params: dict | None = None) -> None:
+    def execute_sql(self, query: Query, params: list | None = None) -> None:
         with self.connection_pool.connection() as conn:
             cursor: Cursor
             with conn.cursor() as cursor:
-                cursor.execute(sql, params=params)
+                cursor.execute(query, params=params)
 
-    def execute_sql_fetch_one(self, sql: Composed, params: dict | None = None) -> dict[str, Any] | None:
+    def execute_sql_fetch_one(self, query: Query, params: list | None = None) -> dict[str, Any] | None:
         with self.connection_pool.connection() as conn:
             cursor: Cursor
             with conn.cursor() as cursor:
-                cursor.execute(sql, params=params)
+                cursor.execute(query, params=params)
                 row = cursor.fetchone()
                 if row is None:
                     return None
                 col_names = [desc[0] for desc in cursor.description]
                 return dict(zip(col_names, row))
 
-    def execute_sql_fetch_all(self, sql: Composed, params: dict | None = None) -> list[dict]:
+    def execute_sql_fetch_all(self, query: Query, params: list | None = None) -> list[dict]:
         with self.connection_pool.connection() as conn:
             cursor: Cursor
             with conn.cursor() as cursor:
-                cursor.execute(sql, params=params)
+                cursor.execute(query, params=params)
                 rows: list = cursor.fetchall()
                 col_names = [desc[0] for desc in cursor.description]
                 return [dict(zip(col_names, row)) for row in rows]
 
-    def select_many(self, table: str) -> list[dict]:
-        sql = SQL('select * from {table}').format(table=Identifier(table))
-        return self.execute_sql_fetch_all(sql)
+    def select_many(
+        self,
+        table: str,
+        fields: list[str],
+        filters: dict[str, Any] | None = None,  # TODO OR filter
+        order_by: list[str] | None = None,
+        desc_order: dict[str, bool] | None = None,
+        limit: int | None = None,
+        offset: int | None = None,
+    ) -> list[dict]:
+        where_clause, where_params = self._build_where_clause(filters)
+        query = SQL('select {fields} from {table}{where}').format(
+            fields=SQL(', ').join(map(Literal, fields)),
+            table=Identifier(table),
+            where=where_clause,
+        )
+        if limit:
+            query += SQL(' limit {}').format(Literal(limit))
+        if offset:
+            query += SQL(' offset {}').format(Literal(offset))
+        if order_by:
+            order_fields = self._build_order_fields(order_by, desc_order)
+            query += SQL(' order by {order_fields}').format(
+                order_fields=SQL(', ').join(order_fields),
+            )
+        return self.execute_sql_fetch_all(query, where_params)
+    
+    def _build_order_fields(
+        self,
+        order_by: list[str] | None,
+        desc_order: dict[str, bool] | None = None,
+    ) -> Iterable[Composable]:
+        for field in order_by or []:
+            if desc_order and desc_order.get(field, False):
+                yield SQL('{} desc').format(Identifier(field))
+            else:
+                yield SQL('{}').format(Identifier(field))
+
+    def select_one(
+        self,
+        table: str,
+        fields: list[str],
+        filters: dict[str, Any] | None = None,
+        order_by: list[str] | None = None,
+        limit: int | None = None,
+        offset: int | None = None,
+    ) -> dict[str, Any] | None:
+        rows = self.select_many(table, fields, filters=filters, order_by=order_by, limit=limit, offset=offset)
+        return rows[0] if rows else None
+
+    def insert_one(
+        self,
+        table: str,
+        data: dict[str, Any],
+    ):
+        query = SQL('insert into {table} ({fields}) values ({values})').format(
+            table=Literal(table),
+            fields=SQL(', ').join(map(Identifier, data.keys())),
+            values=SQL(', ').join(Placeholder() * len(data)),
+        )
+        params = list(data.values())
+        self.execute_sql(query, params)
+
+    def update(
+        self,
+        table: str,
+        data: dict[str, Any],
+        filters: dict[str, Any] | None = None,
+    ):
+        where_clause, where_params = self._build_where_clause(filters)
+        query = SQL('update {table} set {updated_fields}{where}').format(
+            table=Literal(table),
+            updated_fields=SQL(', ').join([SQL('{} = %s').format(Identifier(field)) for field in data.keys()]),
+            where=where_clause,
+        )
+        params = list(data.values()) + where_params
+        self.execute_sql(query, params)
+    
+    def _build_where_clause(self, filters: dict[str, Any] | None = None) -> tuple[Query, list]:
+        if not filters:
+            return SQL(''), []
+        field_conditions = [SQL('{} = %s').format(Identifier(field)) for field in filters.keys()]
+        query = SQL(' where {}').format(
+            SQL(' and ').join(field_conditions)
+        )
+        params = list(filters.values())
+        return query, params
 
 
 def get_database_name() -> str:
