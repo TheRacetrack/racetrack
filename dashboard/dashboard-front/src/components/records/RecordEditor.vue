73965383e8f4208ef70372c9fc9a42627fcec754
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2025-01-23 17:09:02 +0100

    Encode and decode datetimes

--- dashboard/dashboard-front/src/components/records/RecordEditor.vue
+++ dashboard/dashboard-front/src/components/records/RecordEditor.vue
@@ -6,6 +6,7 @@ import {type TableMetadataPayload, type RecordFieldsPayload} from '@/utils/api-s
 import {toastService} from "@/services/ToastService"
 import { mdiDatabase, mdiTable, mdiFileDocumentOutline } from '@quasar/extras/mdi-v7'
 import {progressService} from "@/services/ProgressService"
+import {leftZeroPad} from "@/utils/time";
 
 const route = useRoute()
 const router = useRouter()
@@ -42,7 +43,10 @@ async function fetchRecord(): Promise<void> {
     try {
         let response = await apiClient.get<RecordFieldsPayload>(`/api/v1/records/table/${tableName}/id/${recordId}`)
         recordData.value = response.data
-        inputValues.value = response.data.fields
+        inputValues.value = Object.fromEntries(
+            Object.entries(response.data.fields)
+                .map(([key, value]) => [key, decodeInputValue(value, key)])
+        ) as Record<string, any>
     } catch (err) {
         toastService.showErrorDetails(`Failed to fetch table records`, err)
     } finally {
@@ -51,9 +55,15 @@ async function fetchRecord(): Promise<void> {
 }
 
 function saveRecord() {
+    const allowedTypes = ['str', 'str | None', 'int', 'int | None', 'float', 'float | None', 'datetime', 'datetime | None', 'bool']
+    const fields = Object.fromEntries(
+        Object.entries(inputValues.value)
+            .filter(([key, _]) => allowedTypes.includes(tableMetadata.value.column_types[key]))
+            .map(([key, value]) => [key, encodeInputValue(value, key)])
+    ) as Record<string, any>
     progressService.runLoading({
         task: apiClient.put(`/api/v1/records/table/${tableName}/id/${recordId}`, {
-            fields: inputValues.value,
+            fields: fields,
         }),
         loadingState: submitting,
         progressMsg: `Saving record…`,
@@ -76,6 +86,51 @@ function deleteRecord() {
             router.push({name: 'records-table', params: {table: tableName}})
         },
     })
+}
+
+function decodeInputValue(value: any, fieldName: string): any {
+    const colType = tableMetadata.value.column_types[fieldName]
+    if (colType.endsWith(' | None') && (value == null || value === '')) {
+        return value
+    }
+    if (['datetime', 'datetime | None'].includes(colType)) {
+        const date = new Date(value)
+        const day = leftZeroPad(date.getDate(), 2)
+        const month = leftZeroPad(date.getMonth() + 1, 2)
+        const year = leftZeroPad(date.getFullYear(), 4)
+        const hour = leftZeroPad(date.getHours(), 2)
+        const minute = leftZeroPad(date.getMinutes(), 2)
+        const second = leftZeroPad(date.getSeconds(), 2)
+        const millisecond = leftZeroPad(date.getMilliseconds(), 3)
+        const timezoneOffset = -date.getTimezoneOffset() ?? 0
+        const timezoneHours = leftZeroPad(Math.floor(Math.abs(timezoneOffset) / 60), 2)
+        const timezoneMinutes = leftZeroPad(Math.abs(timezoneOffset) % 60, 2)
+        const timezonePart = timezoneOffset >= 0 ? `+${timezoneHours}:${timezoneMinutes}` : `-${timezoneHours}:${timezoneMinutes}`
+        return `${year}-${month}-${day}, ${hour}:${minute}:${second}.${millisecond} ${timezonePart}`
+    }
+    return value
+}
+
+function encodeInputValue(value: any, fieldName: string): any {
+    const colType = tableMetadata.value.column_types[fieldName]
+    if (colType.endsWith(' | None') && (value == null || value === '')) {
+        return null
+    }
+    if (colType === 'str' && value == null) {
+        return ''
+    }
+    if (['datetime', 'datetime | None'].includes(colType)) {
+        const date = new Date(value)
+        const day = leftZeroPad(date.getUTCDate(), 2)
+        const month = leftZeroPad(date.getUTCMonth() + 1, 2)
+        const year = leftZeroPad(date.getUTCFullYear(), 4)
+        const hour = leftZeroPad(date.getUTCHours(), 2)
+        const minute = leftZeroPad(date.getUTCMinutes(), 2)
+        const second = leftZeroPad(date.getUTCSeconds(), 2)
+        const millisecond = leftZeroPad(date.getUTCMilliseconds(), 3)
+        return `${year}-${month}-${day}T${hour}:${minute}:${second}.${millisecond}+00:00`
+    }
+    return value
 }
 
 onMounted(async () => {
@@ -129,8 +184,8 @@ onMounted(async () => {
                                 name="cancel" @click.stop.prevent="inputValues[fieldName] = null" class="cursor-pointer" />
                         <q-icon name="event" class="cursor-pointer">
                           <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                            <q-date v-model="inputValues[fieldName]" mask="YYYY-MM-DD HH:mm:ss" first-day-of-week="1" today-btn />
-                            <q-time v-model="inputValues[fieldName]" mask="YYYY-MM-DD HH:mm:ss" color="primary" format24h with-seconds now-btn />
+                            <q-date v-model="inputValues[fieldName]" mask="YYYY-MM-DD, HH:mm:ss.SSS Z" first-day-of-week="1" today-btn />
+                            <q-time v-model="inputValues[fieldName]" mask="YYYY-MM-DD, HH:mm:ss.SSS Z" color="primary" format24h with-seconds now-btn />
                             <div class="row items-center justify-end">
                               <q-btn v-close-popup label="Close" color="primary" flat />
                             </div>
