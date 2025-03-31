f6db59172408df409cd04efe51db894b1bfdc48d
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2025-03-31 12:09:31 +0200

    Reimplement the password hasher logic from Django==4.2.17

---
+++ lifecycle/lifecycle/auth/hasher.py
@@ -0,0 +1,285 @@
+"""
+This file reimplements the password hasher logic from Django==4.2.17
+See site-packages/django/contrib/auth/hashers.py
+"""
+import base64
+import functools
+import hashlib
+import importlib
+import math
+import warnings
+import secrets
+import datetime
+
+RANDOM_STRING_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+_PROTECTED_TYPES = (
+    type(None),
+    int,
+    float,
+    datetime.datetime,
+    datetime.date,
+    datetime.time,
+)
+
+
+def make_password(password, salt=None):
+    """
+    Turn a plain-text password into a hash for database storage
+
+    Same as encode() but generate a new random salt. If password is None then
+    return a concatenation of UNUSABLE_PASSWORD_PREFIX and a random string,
+    which disallows logins. Additional random string reduces chances of gaining
+    access to staff or superuser accounts. See ticket #20079 for more info.
+    """
+    if not isinstance(password, (bytes, str)):
+        raise TypeError(
+            "Password must be a string or bytes, got %s." % type(password).__qualname__
+        )
+    hasher = get_hasher()
+    salt = salt or hasher.salt()
+    return hasher.encode(password, salt)
+
+
+@functools.lru_cache
+def get_hasher():
+    hasher = PBKDF2PasswordHasher()
+    if not getattr(hasher, "algorithm"):
+        raise RuntimeError("hasher doesn't specify an algorithm name")
+    return hasher
+
+
+class BasePasswordHasher:
+    """
+    Abstract base class for password hashers
+
+    When creating your own hasher, you need to override algorithm,
+    verify(), encode() and safe_summary().
+
+    PasswordHasher objects are immutable.
+    """
+
+    algorithm = None
+    library = None
+    salt_entropy = 128
+
+    def _load_library(self):
+        if self.library is not None:
+            if isinstance(self.library, (tuple, list)):
+                name, mod_path = self.library
+            else:
+                mod_path = self.library
+            try:
+                module = importlib.import_module(mod_path)
+            except ImportError as e:
+                raise ValueError(
+                    "Couldn't load %r algorithm library: %s"
+                    % (self.__class__.__name__, e)
+                )
+            return module
+        raise ValueError(
+            "Hasher %r doesn't specify a library attribute" % self.__class__.__name__
+        )
+
+    def salt(self):
+        """
+        Generate a cryptographically secure nonce salt in ASCII with an entropy
+        of at least `salt_entropy` bits.
+        """
+        # Each character in the salt provides
+        # log_2(len(alphabet)) bits of entropy.
+        char_count = math.ceil(self.salt_entropy / math.log2(len(RANDOM_STRING_CHARS)))
+        return get_random_string(char_count, allowed_chars=RANDOM_STRING_CHARS)
+
+    def verify(self, password, encoded):
+        """Check if the given password is correct."""
+        raise NotImplementedError(
+            "subclasses of BasePasswordHasher must provide a verify() method"
+        )
+
+    def _check_encode_args(self, password, salt):
+        if password is None:
+            raise TypeError("password must be provided.")
+        if not salt or "$" in salt:
+            raise ValueError("salt must be provided and cannot contain $.")
+
+    def encode(self, password, salt):
+        """
+        Create an encoded database value.
+
+        The result is normally formatted as "algorithm$salt$hash" and
+        must be fewer than 128 characters.
+        """
+        raise NotImplementedError(
+            "subclasses of BasePasswordHasher must provide an encode() method"
+        )
+
+    def decode(self, encoded):
+        """
+        Return a decoded database value.
+
+        The result is a dictionary and should contain `algorithm`, `hash`, and
+        `salt`. Extra keys can be algorithm specific like `iterations` or
+        `work_factor`.
+        """
+        raise NotImplementedError(
+            "subclasses of BasePasswordHasher must provide a decode() method."
+        )
+
+    def safe_summary(self, encoded):
+        """
+        Return a summary of safe values.
+
+        The result is a dictionary and will be used where the password field
+        must be displayed to construct a safe representation of the password.
+        """
+        raise NotImplementedError(
+            "subclasses of BasePasswordHasher must provide a safe_summary() method"
+        )
+
+    def must_update(self, encoded):
+        return False
+
+    def harden_runtime(self, password, encoded):
+        """
+        Bridge the runtime gap between the work factor supplied in `encoded`
+        and the work factor suggested by this hasher.
+
+        Taking PBKDF2 as an example, if `encoded` contains 20000 iterations and
+        `self.iterations` is 30000, this method should run password through
+        another 10000 iterations of PBKDF2. Similar approaches should exist
+        for any hasher that has a work factor. If not, this method should be
+        defined as a no-op to silence the warning.
+        """
+        warnings.warn(
+            "subclasses of BasePasswordHasher should provide a harden_runtime() method"
+        )
+
+
+class PBKDF2PasswordHasher(BasePasswordHasher):
+    """
+    Secure password hashing using the PBKDF2 algorithm (recommended)
+
+    Configured to use PBKDF2 + HMAC + SHA256.
+    The result is a 64 byte binary string.  Iterations may be changed
+    safely but you must rename the algorithm if you change SHA256.
+    """
+
+    algorithm = "pbkdf2_sha256"
+    iterations = 600000
+    digest = hashlib.sha256
+
+    def encode(self, password, salt, iterations=None):
+        self._check_encode_args(password, salt)
+        iterations = iterations or self.iterations
+        hash = pbkdf2(password, salt, iterations, digest=self.digest)
+        hash = base64.b64encode(hash).decode("ascii").strip()
+        return "%s$%d$%s$%s" % (self.algorithm, iterations, salt, hash)
+
+    def decode(self, encoded):
+        algorithm, iterations, salt, hash = encoded.split("$", 3)
+        assert algorithm == self.algorithm
+        return {
+            "algorithm": algorithm,
+            "hash": hash,
+            "iterations": int(iterations),
+            "salt": salt,
+        }
+
+    def verify(self, password, encoded):
+        decoded = self.decode(encoded)
+        encoded_2 = self.encode(password, decoded["salt"], decoded["iterations"])
+        return constant_time_compare(encoded, encoded_2)
+
+    def safe_summary(self, encoded):
+        decoded = self.decode(encoded)
+        return {
+            "algorithm": decoded["algorithm"],
+            "iterations": decoded["iterations"],
+            "salt": mask_hash(decoded["salt"]),
+            "hash": mask_hash(decoded["hash"]),
+        }
+
+    def must_update(self, encoded):
+        decoded = self.decode(encoded)
+        update_salt = must_update_salt(decoded["salt"], self.salt_entropy)
+        return (decoded["iterations"] != self.iterations) or update_salt
+
+    def harden_runtime(self, password, encoded):
+        decoded = self.decode(encoded)
+        extra_iterations = self.iterations - decoded["iterations"]
+        if extra_iterations > 0:
+            self.encode(password, decoded["salt"], extra_iterations)
+
+
+def get_random_string(length, allowed_chars=RANDOM_STRING_CHARS):
+    """
+    Return a securely generated random string.
+
+    The bit length of the returned value can be calculated with the formula:
+        log_2(len(allowed_chars)^length)
+
+    For example, with default `allowed_chars` (26+26+10), this gives:
+      * length: 12, bit length =~ 71 bits
+      * length: 22, bit length =~ 131 bits
+    """
+    return "".join(secrets.choice(allowed_chars) for i in range(length))
+
+
+def pbkdf2(password, salt, iterations, dklen=0, digest=None):
+    """Return the hash of password using pbkdf2."""
+    if digest is None:
+        digest = hashlib.sha256
+    dklen = dklen or None
+    password = force_bytes(password)
+    salt = force_bytes(salt)
+    return hashlib.pbkdf2_hmac(digest().name, password, salt, iterations, dklen)
+
+
+def force_bytes(s, encoding="utf-8", strings_only=False, errors="strict"):
+    """
+    Similar to smart_bytes, except that lazy instances are resolved to
+    strings, rather than kept as lazy objects.
+
+    If strings_only is True, don't convert (some) non-string-like objects.
+    """
+    # Handle the common case first for performance reasons.
+    if isinstance(s, bytes):
+        if encoding == "utf-8":
+            return s
+        else:
+            return s.decode("utf-8", errors).encode(encoding, errors)
+    if strings_only and is_protected_type(s):
+        return s
+    if isinstance(s, memoryview):
+        return bytes(s)
+    return str(s).encode(encoding, errors)
+
+
+def is_protected_type(obj):
+    """Determine if the object instance is of a protected type.
+
+    Objects of protected types are preserved as-is when passed to
+    force_str(strings_only=True).
+    """
+    return isinstance(obj, _PROTECTED_TYPES)
+
+
+def constant_time_compare(val1, val2):
+    """Return True if the two strings are equal, False otherwise."""
+    return secrets.compare_digest(force_bytes(val1), force_bytes(val2))
+
+
+def mask_hash(hash, show=6, char="*"):
+    """
+    Return the given hash, with only the first ``show`` number shown. The
+    rest are masked with ``char`` for security reasons.
+    """
+    masked = hash[:show]
+    masked += char * len(hash[show:])
+    return masked
+
+
+def must_update_salt(salt, expected_entropy):
+    # Each character in the salt provides log_2(len(alphabet)) bits of entropy.
+    return len(salt) * math.log2(len(RANDOM_STRING_CHARS)) < expected_entropy
